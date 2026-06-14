9723f186b3ce3c5e904b470b1048dfcc2d2d9172
tsepiso2 <tsepisomonokoane24@gmail.com>
2026-06-14 03:38:23 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -15,4 +15,4 @@ export default defineConfig({
       '@shared': path.resolve(__dirname, './shared'),
     },
   },
-});
+})
